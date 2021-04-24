bf00859c61644454a203db69dd10adb7f24efa80
Creator William <creator_william@icloud.com>
2021-04-24 18:05:23 +0800

    指定支持的平台

--- Package.swift
+++ Package.swift
@@ -4,26 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "SQLiteKit",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SQLiteKit",
-            type: .static,
-            targets: ["SQLiteKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SQLiteKit",
-            dependencies: []),
-        .testTarget(
-            name: "SQLiteKitTests",
-            dependencies: ["SQLiteKit"]),
-    ]
+  name: "SQLiteKit",
+  platforms: [.iOS(.v9), .macOS(.v10_10)],
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "SQLiteKit",
+      type: .static,
+      targets: ["SQLiteKit"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target(
+      name: "SQLiteKit",
+      dependencies: []),
+    .testTarget(
+      name: "SQLiteKitTests",
+      dependencies: ["SQLiteKit"]),
+  ]
 )
